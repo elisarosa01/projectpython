51e8bcc050246c5a651b394ab1fa6558a028deb0
elisarosa01 <elisa.rosa@studenti.unimi.it>
2024-11-25 13:05:51 +0100

    Update utilities.py

--- project python weather/utilities.py
+++ project python weather/utilities.py
@@ -1,5 +1,1 @@
-class Utils:
-    @staticmethod
-    def calculate_std(data):
-        """Calculates temperature standard deviation."""
-        return data.groupby(['City', 'Country'])['AverageTemperature'].std().reset_index()
+
